df3b202ddd9127e9c9b33194be9026ad3e4f53c7
Florian Plank <polarblau@gmail.com>
2014-07-21 23:36:58 +0200

    Import.

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -1,1 +1,12 @@
 //= require 'vendor/jquery-1.10.2.min'
+//= require 'vendor/mediaelement-and-player'
+
+$('video').mediaelementplayer({
+  pluginPath: '/plugins/',
+  iPadUseNativeControls: true,
+  iPhoneUseNativeControls: true,
+  AndroidUseNativeControls: true,
+  success: function(media, node, player) {
+    console.log("Video::success");
+  }
+});
